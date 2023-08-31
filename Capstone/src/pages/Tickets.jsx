f411c484ad1e9d8457c15b55c5d21ab66fa92f6f
Talani Balogun <Taloni5992@gmail.com>
2023-08-31 17:32:56 -0400

    initial files

--- Capstone/src/pages/Tickets.jsx
+++ Capstone/src/pages/Tickets.jsx
@@ -5,19 +5,22 @@ export default  function Tickets() {
     const [tickets, setTickets]= useState([]);
     const [name, setName]= useState('')
     const [text, setText]=useState('')
-    const [imageurl, setImageUrl]=useState('')
+    const [count, setCount]=useState(1)
 
     
     return (
        
-       <div>
+       <>
         <h1>Tickets</h1>
      <form>
 
      </form>
      <button>Add To Cart</button>
+     <div className="quantity">
+      
+     </div>
      <select>Quantity</select>
-        </div>
+        </>
     
     )
 }
